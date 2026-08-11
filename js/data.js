4854a560aeff0e980aae0f114c73c9d59ac3a6a2
borobasra-crypto <borobasra@gmail.com>
2026-08-11 20:13:00 +0600

    Add new prompt entries to data.js
    Added new prompts with details including title, date, categories, image, description, prompt text, premium status, and ad limit.

--- js/data.js
+++ js/data.js
@@ -4,5 +4,34 @@
 // The post will automatically appear in those category pages.
 export const categories=['New','All','Trending','Boy','Girl','Premium','Thumbnail','Outfit','Filter','Cinematic','Realistic'];
 export const prompts=[
-
+{
+  "id": 1,
+  "title": "Sinama",
+  "date": "2026-08-11",
+  "categories": [
+    "Trending",
+    "Boy",
+    "Premium",
+    "Thumbnail"
+  ],
+  "image": "https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg",
+  "description": "Desc",
+  "prompt": "Promtsnsnsnsnns",
+  "premium": false
+}
+  {
+  "id": 2,
+  "title": "Sinama",
+  "date": "2026-08-11",
+  "categories": [
+    "Trending",
+    "Boy",
+    "Thumbnail"
+  ],
+  "image": "https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg",
+  "description": "Desc",
+  "prompt": "Promtsnsnsnsnns",
+  "premium": true,
+  "adLimit": 2
+}
 ];
