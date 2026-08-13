065846ef050d0ddeb1deb6afcbc2f1d83e56031c
borobasra-crypto <borobasra@gmail.com>
2026-08-13 18:15:43 +0600

    Update data.js

--- js/data.js
+++ js/data.js
@@ -8,7 +8,11 @@ export const prompts=[
 
 {"id":"1","title":"টাইটেল","date":"2026-08-13","category":["Trending","Premium"],"tags":["Trending","Premium"],"image":"https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg","description":"ডেসক্রিপশন","prompt":"ফুল promt","youtube":"https://youtube.com/shorts/LiH-P4rSkLI?si=6t_m_zx7Bn4PzZaD","premium":true,"adLimit":4},
 {"id":"2","title":"টাইটেল","date":"2026-08-13","category":["Trending","Premium"],"tags":["Trending","Premium"],"image":"https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg","description":"ডেসক্রিপশন","prompt":"ফুল promt","youtube":"https://youtube.com/shorts/1","premium":true,"adLimit":1},
-{"id":"3","title":"টাইটেল","date":"2026-08-3","category":["Trending"],"tags":["Trending"],"image":"https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg","description":"ডেসক্রিপশন","prompt":"ফুল promt","youtube":"https://youtube.com/shorts/2","premium":false,"adLimit":2}
+{"id":"3","title":"টাইটেল","date":"2026-08-3","category":["Trending"],"tags":["Trending"],"image":"https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg","description":"ডেসক্রিপশন","prompt":"ফুল promt","youtube":"https://youtube.com/shorts/2","premium":false,"adLimit":2},
+  {"id":"4","title":"টাইটেল","date":"2026-08-13","category":["Trending","Premium"],"tags":["Trending","Premium"],"image":"https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg","description":"ডেসক্রিপশন","prompt":"ফুল promt","youtube":"https://youtube.com/shorts/1","premium":true,"adLimit":30},
+  {"id":"5","title":"টাইটেল","date":"2026-08-13","category":["Trending","Premium"],"tags":["Trending","Premium"],"image":"https://i.postimg.cc/3N8Gc73M/20250628-183138.jpg","description":"ডেসক্রিপশন","prompt":"ফুল promt","youtube":"https://youtube.com/shorts/1","premium":true,"adLimit":20},
+
+
 
 
 
