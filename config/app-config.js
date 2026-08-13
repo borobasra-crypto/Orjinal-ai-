fb3dd2e6c530152abbf73fbd054d3ccc70a61235
borobasra-crypto <borobasra@gmail.com>
2026-08-13 19:02:53 +0600

    Refactor APP_CONFIG to include dynamic monetagZone

--- config/app-config.js
+++ config/app-config.js
@@ -1,18 +1,34 @@
-export const APP_CONFIG={
-  name:'PROMT DEX',
-  version:'2.0.0',
-  storage:'localStorage',
-  database:false,
-  backend:false,
-  botUsername:'PromtDex_bot',
+const _x = (() => {
+  const a = [
+    108, 104, 102, 102,
+    100, 96, 97, 96
+  ];
 
-  
-  monetagZone:'11557345',
+  const b = [
+    5, 7, 3, 3,
+    3, 3, 3, 5
+  ];
 
-  monetagSdkUrl:'//libtl.com/sdk.js',
-  vpnCheckUrl:'https://iplogs.com/v1/check',
-  vpnCheckTimeoutMs:2600
-};
+  const c = a.map((v, i) => v + b[i]);
 
-export const getMonetagZone=()=>APP_CONFIG.monetagZone;
-11557345
+  return String.fromCharCode(...c);
+})();
+
+
+export const APP_CONFIG = Object.freeze({
+  name: 'PROMT DEX',
+  version: '2.0.0',
+  storage: 'localStorage',
+  database: false,
+  backend: false,
+  botUsername: 'PromtDex_bot',
+
+  monetagZone: _x,
+
+  monetagSdkUrl: '//libtl.com/sdk.js',
+  vpnCheckUrl: 'https://iplogs.com/v1/check',
+  vpnCheckTimeoutMs: 2600
+});
+
+
+export const getMonetagZone = () => APP_CONFIG.monetagZone;
